e68dc89463736e1a07743a7853a4a5aaf6ecfc51
YuvRaj6392 <74025229+YuvRaj6392@users.noreply.github.com>
2024-07-12 19:30:10 +0530

    added

---
+++ eslint-express/src/index.ts
@@ -0,0 +1,18 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({
+    message: "Hi there",
+  });
+});
+
+function withSpace (x: number) {
+  console.log(x)
+}
+
+withSpace(1)
+
+//npm init @eslint/config@latest
+//npx eslint . --fix
